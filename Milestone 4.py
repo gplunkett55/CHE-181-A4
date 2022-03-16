fa1d233e1c62808b446151e90fc7121e13e47f0f
Gavin Plunkett <44099462+gplunkett55@users.noreply.github.com>
2022-03-16 12:45:53 -0400

    Cleaned up the file

--- Milestone 4.py
+++ Milestone 4.py
@@ -1,25 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pyfirmata as pf
 from jupyterplot import ProgressPlot
 from time import sleep
-
-
-# In[2]:
-
 
 #Set up arduino (only needs to be run once)
 board = pf.Arduino("COM5")
 it = pf.util.Iterator(board)
 it.start()
-
-
-# In[3]:
-
 
 #Defines the a0 pin on the arduino
 a0 = board.get_pin('a:0:i')
@@ -27,16 +13,8 @@ a1 = board.get_pin('a:1:i')
 a2 = board.get_pin('a:2:i')
 a3 = board.get_pin('a:3:i')
 
-
-# In[4]:
-
-
 #Calculates the voltage in thee a0 pin
 voltage = a0.read() * 5
-
-
-# In[10]:
-
 
 #Creates the plot and also records data to a .txt file
 with open('temperature.txt', 'w') as f:
@@ -58,37 +36,3 @@ with open('temperature.txt', 'w') as f:
         f.write(str(time)+'\t'+str(temperature)+'\n')
     pp.finalize()
         
-
-
-# In[ ]:
-
-
-f = open('data.txt', 'w')
-
-
-# In[ ]:
-
-
-for i in range(20):
-    f.write(str(i)+'\n')
-
-
-# In[ ]:
-
-
-f.close()
-
-
-# In[ ]:
-
-
-with open('data.txt', 'w') as f:
-    for i in range(300):
-        f.write(str(i)+'\t'+str(i**2)+'\n')
-
-
-# In[ ]:
-
-
-
-
